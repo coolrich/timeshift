09188ef11d7cb13b12994235d8211e1c4b823edb
CraftBox <i.domnich2017@gmail.com>
2025-06-13 16:05:13 +0300

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(qpotno#_iop^3fehd1t6gy^vaz^(meu%k^4&cduvp%pkg#r2z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home_page_view
+# from .views import home_page_view
+from .views import HomePageView, AboutPageView
+
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
+    # path("", home_page_view, name="home"),
     # path("home/", home_page_view, name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
+
 ]
