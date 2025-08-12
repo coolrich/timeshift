fdc22881d0af4b26847156450e01e090a3b867e4
CraftBox <i.domnich2017@gmail.com>
2025-08-12 13:45:30 +0300

    - Added "accounts" app and URLs. - Users can now register on the site.

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp.apps.PagesConfig'
+    'webapp.apps.PagesConfig',
+    "accounts",
 ]
 
 MIDDLEWARE = [
