62726b7ba9e941f631b0b68848ba07ff194c56d6
CraftBox <i.domnich2017@gmail.com>
2025-10-28 19:57:36 +0200

    - Implementation of API. Stage of fixing and testing:   - small changes in reqirements.txt and settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # from django.conf.global_settings import LOGIN_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(qpotno#_iop^3fehd1t6gy^vaz^(meu%k^4&cduvp%pkg#r2z'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
