8b245d37168257d1fe4bd735a4f5da366914c857
CraftBox <i.domnich2017@gmail.com>
2025-10-29 20:16:33 +0200

    - Implementation of API. Stage of fixing and testing:   - changes in wsgi.py

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
-import post_deploy
+from django_project.post_deploy import run
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
 
 call_command('collectstatic', '--noinput')
-post_deploy.run()
+run()
