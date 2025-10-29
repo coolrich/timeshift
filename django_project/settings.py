319d13f5f2f97f8c1aea1c07e557cd75f9111e6a
CraftBox <i.domnich2017@gmail.com>
2025-10-29 19:00:27 +0200

    - Implementation of API. Stage of fixing and testing:   - changed DEBUG to false in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO: write code that makes DEBUG = True for local development and False for production
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*",  'localhost', '127.0.0.1']
 
