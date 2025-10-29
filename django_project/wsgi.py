f1b4291287cb69cc38f75aa2cc485496fdb81bab
CraftBox <i.domnich2017@gmail.com>
2025-10-29 15:00:55 +0200

    - Implementation of API. Stage of fixing and testing:   - added init commands in wsgi.py

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
+
+django.setup()
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migrations skipped or failed: {e}")
