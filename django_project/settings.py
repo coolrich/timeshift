2ad0fc0593b40e24a743f301cc3cbae6f583622a
CraftBox <i.domnich2017@gmail.com>
2025-11-18 21:19:06 +0200

    Developing of user profile:     - Changed database from sqlite to postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -101,13 +101,22 @@ if os.environ.get('DATABASE_URL'):
         )
     }
 else:
-    # Локально — SQLite
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("PG_NAME"),
+            'USER': os.getenv("PG_NAME"),
+            'PASSWORD': os.getenv("PG_PASSWORD"),
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
